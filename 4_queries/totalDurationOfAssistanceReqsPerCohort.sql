10db0286bc803dc0b37a7a20edad0c4ac8429724
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-03-10 17:11:14 +0000

    Total duration per assistance request for each cohort

---
+++ 4_queries/totalDurationOfAssistanceReqsPerCohort.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohort, sum(completed_at - started_at) AS total_duration
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON  students.cohort_id = cohorts.id
+GROUP BY cohort
+ORDER BY total_duration;
