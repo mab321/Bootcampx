fa9ac0df2c9c200059abc4f847a27fd4d5e347ba
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-03-10 17:41:20 +0000

    each assignments with the total number of assistance requests DESC

---
+++ 4_queries/mostConfusingAssignments.sql
@@ -0,0 +1,5 @@
+SELECT assignments.id AS id, name, day, chapter, count(assistance_requests.*) AS total_requests
+FROM assistance_requests
+JOIN assignments ON assignments.id = assistance_requests.assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
